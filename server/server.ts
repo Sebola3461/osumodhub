aa61e26b9daa2afc7de6a84badd465b7af1f1bdd
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-06-08 20:21:45 -0300

    prod

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import express, { Handler, json } from "express";
-import path from "path";
 import "../database";
 import { ApiRoutes } from "./api/routes";
 import DiscordEmbed from "./middlewares/DiscordEmbed";
@@ -8,9 +7,6 @@ const app = express();
 
 app.use(json());
 app.get("*", DiscordEmbed);
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname.concat("./../index.html")));
-});
 app.use("/api/", ApiRoutes);
 
 export default app;
