d0537d169676fadfa5aad4bf34be2e411c43925b
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-06-16 15:11:08 -0300

    add discord invite

--- src/components/global/AppBar.tsx
+++ src/components/global/AppBar.tsx
@@ -26,22 +26,26 @@ export default () => {
         <Link to="/" className="anchor1 page-anchor">
           Home
         </Link>
-        <Link to="/" className="anchor1 page-anchor">
+        <a href="https://discord.gg/fpE4YmtRqz" className="anchor1 page-anchor">
           Discord
-        </Link>
+        </a>
       </div>
       <div className="right-content">
-        { login._id == -1 ? <button onClick={StartAuthentication}>Log-in</button> : <div
-          className="avatar"
-          onClick={() => {
-            login._id == -1
-              ? StartAuthentication()
-              : sideMenuContext.setOpen(!sideMenuContext.open);
-          }}
-          style={{
-            backgroundImage: `url(https://a.ppy.sh/${login._id})`,
-          }}
-        ></div>}
+        {login._id == -1 ? (
+          <button onClick={StartAuthentication}>Log-in</button>
+        ) : (
+          <div
+            className="avatar"
+            onClick={() => {
+              login._id == -1
+                ? StartAuthentication()
+                : sideMenuContext.setOpen(!sideMenuContext.open);
+            }}
+            style={{
+              backgroundImage: `url(https://a.ppy.sh/${login._id})`,
+            }}
+          ></div>
+        )}
       </div>
     </div>
   );
