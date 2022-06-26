035a34dbb2037fcd3fb82fb2c3a7e7622611942e
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-06-26 17:11:17 -0300

    fix embed

--- server/api/queues/GenerateOgImage.ts
+++ server/api/queues/GenerateOgImage.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import jimp from "jimp";
+import { resolve } from "path";
 import { queues } from "../../../database";
 
 export default async (req: Request, res: Response) => {
@@ -19,10 +20,28 @@ export default async (req: Request, res: Response) => {
 
     // ? Background
     const banner = await jimp.read(queue.banner);
+    const bannerClone = banner.clone();
     banner.resize(1280, 720);
 
+    const ratio = getCoverSize(
+      bannerClone.getWidth(),
+      bannerClone.getHeight(),
+      1280,
+      720
+    );
+    bannerClone.resize(ratio.width, ratio.height);
+
+    const scale = Math.min(1280 / banner.getWidth(), 720 / banner.getHeight());
+    const w = banner.getWidth() * scale;
+    const h = banner.getHeight() * scale;
+    banner.composite(bannerClone, 1280 / 2 - w / 2, 720 / 2 - h / 2);
+
     // ? Background gradient
-    const gradient = await jimp.read(`./server/files/png/gradient.png`);
+    const gradient = await jimp.read(
+      __dirname != undefined
+        ? resolve(__dirname + `/../../files/png/gradient.png`)
+        : `./server/files/png/gradient.png`
+    );
     gradient.resize(1280, 720);
     banner.composite(gradient, 0, 0);
 
@@ -30,7 +49,11 @@ export default async (req: Request, res: Response) => {
     const avatar = await jimp.read(`https://a.ppy.sh/${queue_id}`);
     avatar.resize(sizes.avatar.w, sizes.avatar.h);
 
-    const avatarMask = await jimp.read("./server/files/png/round_mask.png");
+    const avatarMask = await jimp.read(
+      __dirname != undefined
+        ? resolve(__dirname + "/../../files/png/round_mask.png")
+        : "./server/files/png/round_mask.png"
+    );
     avatarMask.resize(sizes.avatar.w, sizes.avatar.h);
     avatar.mask(avatarMask, 0, 0);
     banner.composite(avatar, 100, 100);
@@ -40,19 +63,31 @@ export default async (req: Request, res: Response) => {
     let x = 100;
     let y = 320;
 
-    const font = await jimp.loadFont("./server/files/font/quicksand.fnt");
+    const font = await jimp.loadFont(
+      __dirname != undefined
+        ? resolve(__dirname + "/../../files/font/quicksand.fnt")
+        : "./server/files/font/quicksand.fnt"
+    );
     banner.print(font, x, y, message);
 
     // ? Type
     const type = await jimp.read(
-      `./server/files/png/${queue.type}tag.png`.toLowerCase()
+      __dirname != undefined
+        ? resolve(
+            __dirname + `/../../files/png/${queue.type}tag.png`.toLowerCase()
+          )
+        : `./server/files/png/${queue.type}tag.png`.toLowerCase()
     );
     type.resize(300, 300);
     banner.composite(type, 48, 300);
 
     // ? Status
     const status = await jimp.read(
-      `./server/files/png/${queue.open ? "open" : "closed"}.png`
+      __dirname != undefined
+        ? resolve(
+            __dirname + `/../../files/png/${queue.open ? "open" : "closed"}.png`
+          )
+        : `./server/files/png/${queue.open ? "open" : "closed"}.png`
     );
     status.resize(300, 300);
     banner.composite(status, 900, 0);
@@ -61,6 +96,32 @@ export default async (req: Request, res: Response) => {
     res.setHeader("content-type", "image/png");
     const buffer = await banner.getBufferAsync("image/png");
     res.send(buffer);
+
+    function getCoverSize(
+      imgWidth: number,
+      imgHeight: number,
+      containerWidth: number,
+      containerHeight: number
+    ) {
+      const imgRatio = imgHeight / imgWidth; // original img ratio
+      const containerRatio = containerHeight / containerWidth; // container ratio
+
+      let finalWidth = 0;
+      let finalHeight = 0;
+
+      if (containerRatio > imgRatio) {
+        finalHeight = containerHeight;
+        finalWidth = containerHeight / imgRatio;
+      } else {
+        finalWidth = containerWidth;
+        finalHeight = containerWidth / imgRatio;
+      }
+
+      return {
+        width: finalWidth,
+        height: finalHeight,
+      };
+    }
   } catch (e) {
     console.error(e);
     res.status(500);
