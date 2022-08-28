ed634ea47c14cc567153d5c90b5c0514daaaa292
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-08-28 15:10:51 -0300

    Fix notification deletion

--- server/api/notifications/routes.ts
+++ server/api/notifications/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import ClearUserNotifications from "../users/ClearUserNotifications";
+import ClearUserNotifications from "./ClearUserNotifications";
 import GetUserNotifications from "./GetUserNotifications";
+import ValidateNotification from "./ValidateNotification";
 
 const router = Router();
 
 // ? =============== GET REQUESTS
 router.get("/", GetUserNotifications);
+
+// ? =============== POST REQUESTS
+router.post("/:notification", ValidateNotification);
 
 // ? =============== DELETE REQUESTS
 router.delete("/", ClearUserNotifications);
