765888e33559575f5042b84c1c9e0f9f65ea36a4
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-08-29 13:40:36 -0300

    Fix discord url

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,13 @@
-import express, { Handler, json } from "express";
+import express, {
+  Handler,
+  json,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import path from "path";
 import "../database";
+import { queues } from "../database";
 import { ApiRoutes } from "./api/routes";
 import { ClientRoutes } from "./client/routes";
 import { consoleCheck } from "./helpers/logger";
@@ -20,7 +27,7 @@ if (process.env.NODE_ENV == "production") {
   );
 
   app.use(json());
-  app.get("*", DiscordEmbed);
+  app.get("*", rewriteUsernameToId, DiscordEmbed);
   app.use("/api/", ApiRoutes);
   app.use("*", ClientRoutes);
 
@@ -31,6 +38,32 @@ if (process.env.NODE_ENV == "production") {
   app.use(json());
   app.get("*", DiscordEmbed);
   app.use("/api/", ApiRoutes);
+}
+
+async function rewriteUsernameToId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const target = req.params.queue;
+
+  if (isNaN(Number(target))) return rewrite();
+
+  async function rewrite() {
+    const targetQueue = await queues.findOne({ name: target });
+
+    if (!targetQueue)
+      return res.status(404).send({
+        status: 404,
+        message: "Queue not found!",
+      });
+
+    req.params.queue = targetQueue._id;
+
+    return next();
+  }
+
+  return next();
 }
 
 export default app;
