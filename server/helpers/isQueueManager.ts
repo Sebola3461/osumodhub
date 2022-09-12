b9440fa502aaa64b6752ef25b768de4cef8fd4ef
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-09-11 23:29:23 -0300

    Fix queue admin check

--- server/helpers/isQueueManager.ts
+++ server/helpers/isQueueManager.ts
@@ -2,11 +2,11 @@ import { IQueue } from "./../../src/types/queue";
 
 export default (queue: IQueue, user: any, authorization: string) => {
   if (queue.isGroup) {
-    if (user._id == queue.owner) return true;
+    if (user._id.toString() == queue.owner) return true;
 
-    if (queue.admins.includes(user._id)) return true;
+    if (queue.admins.includes(user._id.toString())) return true;
   } else {
-    if (user._id != queue.owner) return false;
+    if (user._id.toString() != queue.owner) return false;
 
     if (user.account_token != authorization) return false;
 
--- src/helpers/isQueueManager.ts
+++ src/helpers/isQueueManager.ts
@@ -3,7 +3,7 @@ import { IQueue } from "../types/queue";
 export default (queue: IQueue, login: any) => {
   if (login._id == queue.owner) return true;
 
-  if (queue.admins.includes(login._id)) return true;
+  if (queue.admins.includes(login._id.toString())) return true;
 
   return false;
 };
