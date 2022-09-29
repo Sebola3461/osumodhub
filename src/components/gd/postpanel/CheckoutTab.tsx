19120041d9cf9f65ca4650f9fa5104834773ffb2
Sebola3461 <mixvideostubecontato@gmail.com>
2022-09-29 12:54:15 -0300

    aaaa

--- src/components/gd/postpanel/CheckoutTab.tsx
+++ src/components/gd/postpanel/CheckoutTab.tsx
@@ -1,56 +1,59 @@
-import { useState, useEffect, useContext } from "react";
-import { AuthContext } from "../../../providers/AuthContext";
-import BeatmapSelector from "../../global/BeatmapSelector";
-import TagsInput from "../../global/TagsInput";
-import GDSelector from "../GDSelector";
+// import { useState, useEffect, useContext } from "react";
+// import { AuthContext } from "../../../providers/AuthContext";
+// import BeatmapSelector from "../../global/BeatmapSelector";
+// import TagsInput from "../../global/TagsInput";
+// import GDSelector from "../GDSelector";
 
-export default ({
-  post,
-  setPost,
-  loading,
-  setLoading,
-  submit,
-}: {
-  post: any;
-  setPost: any;
-  loading: boolean;
-  setLoading: any;
-  submit: any;
-}) => {
-  return (
-    <div className={loading ? "checkouttab loading" : "checkouttab"}>
-      <GDSelector request={post}></GDSelector>
-      <div className="actions">
-        <textarea
-          className="comment"
-          placeholder="Type about your beatmap"
-        ></textarea>
-        <div className="__container">
-          <p className="__container-title">Genres</p>
-          <TagsInput
-            value={post.genres}
-            onInput={(genres) => {
-              post.genres = genres;
+// export default ({
+//   post,
+//   setPost,
+//   loading,
+//   setLoading,
+//   submit,
+// }: {
+//   post: any;
+//   setPost: any;
+//   loading: boolean;
+//   setLoading: any;
+//   submit: any;
+// }) => {
+//   return (
+//     <div className={loading ? "checkouttab loading" : "checkouttab"}>
+//       <GDSelector request={post}></GDSelector>
+//       <div className="actions">
+//         <textarea
+//           className="comment"
+//           placeholder="Type about your beatmap"
+//         ></textarea>
+//         <div className="__container">
+//           <p className="__container-title">Genres</p>
+//           <TagsInput
+//             value={post.genres}
+//             onInput={(genres) => {
+//               post.genres = genres;
 
-              setPost(post);
-            }}
-          ></TagsInput>
-        </div>
-        <div className="__container">
-          <p className="__container-title">Tags</p>
-          <TagsInput
-            value={post.tags}
-            onInput={(tags) => {
-              post.tags = tags;
+//               setPost(post);
+//             }}
+//           ></TagsInput>
+//         </div>
+//         <div className="__container">
+//           <p className="__container-title">Tags</p>
+//           <TagsInput
+//             value={post.tags}
+//             onInput={(tags) => {
+//               post.tags = tags;
 
-              setPost(post);
-            }}
-          ></TagsInput>
-        </div>
-        <button className="submit" onClick={submit}>
-          Submit
-        </button>
-      </div>
-    </div>
-  );
+//               setPost(post);
+//             }}
+//           ></TagsInput>
+//         </div>
+//         <button className="submit" onClick={submit}>
+//           Submit
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+export default () => {
+  return <></>;
 };
--- src/components/gd/postpanel/SelectBeatmap.tsx
+++ src/components/gd/postpanel/SelectBeatmap.tsx
@@ -1,39 +1,42 @@
-import { useState, useEffect, useContext } from "react";
-import { AuthContext } from "../../../providers/AuthContext";
-import BeatmapSelector from "../../global/BeatmapSelector";
+// import { useState, useEffect, useContext } from "react";
+// import { AuthContext } from "../../../providers/AuthContext";
+// import BeatmapSelector from "../../global/BeatmapSelector";
 
-export default ({
-  beatmaps,
-  setTab,
-  selected,
-  setSelected,
-  post,
-  setPost,
-}: {
-  beatmaps: any[];
-  setTab: any;
-  selected: number;
-  setSelected: any;
-  post: any;
-  setPost: any;
-}) => {
-  return (
-    <div className="beatmapstab">
-      {beatmaps.map((b) => {
-        return (
-          <BeatmapSelector
-            beatmapset={b}
-            selected={selected}
-            onClick={() => {
-              setSelected(b.id);
-              setTab(1);
+// export default ({
+//   beatmaps,
+//   setTab,
+//   selected,
+//   setSelected,
+//   post,
+//   setPost,
+// }: {
+//   beatmaps: any[];
+//   setTab: any;
+//   selected: number;
+//   setSelected: any;
+//   post: any;
+//   setPost: any;
+// }) => {
+//   return (
+//     <div className="beatmapstab">
+//       {beatmaps.map((b) => {
+//         return (
+//           <BeatmapSelector
+//             beatmapset={b}
+//             selected={selected}
+//             onClick={() => {
+//               setSelected(b.id);
+//               setTab(1);
 
-              post.beatmap = b;
-              setPost(post);
-            }}
-          />
-        );
-      })}
-    </div>
-  );
+//               post.beatmap = b;
+//               setPost(post);
+//             }}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// };
+export default () => {
+  return <></>;
 };
