0faa94ea2f27b4340c50378832f760bd4d9f35ec
Sebola3461 <mixvideostubecontato@gmail.com>
2022-09-29 12:51:17 -0300

    eae

--- src/components/gd/GDPanel.tsx
+++ src/components/gd/GDPanel.tsx
@@ -1,191 +1,194 @@
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useSnackbar } from "notistack";
-import { useContext, useEffect, useState } from "react";
-import { GenerateComponentKey } from "../../helpers/GenerateComponentKey";
-import { AuthContext } from "../../providers/AuthContext";
-import { GDPanelContext } from "../../providers/GDPanelContext";
-import "./../../styles/GDPanel.css";
-import SelectDifficulty from "./gdpanel/SelectDifficulty";
+// import { faTimes } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { useSnackbar } from "notistack";
+// import { useContext, useEffect, useState } from "react";
+// import { GenerateComponentKey } from "../../helpers/GenerateComponentKey";
+// import { AuthContext } from "../../providers/AuthContext";
+// import { GDPanelContext } from "../../providers/GDPanelContext";
+// import "./../../styles/GDPanel.css";
+// import SelectDifficulty from "./gdpanel/SelectDifficulty";
 
+// export default () => {
+//   const [tab, setTab] = useState(0);
+//   const [beatmaps, setBeatmaps] = useState([]);
+//   const [offset, setOffset] = useState(0);
+//   const [selectedBeatmap, setSelectedBeatmap] = useState(0);
+//   const { login, setLogin } = useContext(AuthContext);
+
+//   const [selectedDifficulty, setSelectedDifficulty] = useState(-1);
+//   const [canSubmit, setCanSubmit] = useState(false);
+//   const context = useContext(GDPanelContext);
+//   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+//   const [loading, setLoading] = useState(false);
+
+//   const action = (key) => (
+//     <>
+//       <button
+//         onClick={() => {
+//           closeSnackbar(key);
+//         }}
+//       >
+//         X
+//       </button>
+//     </>
+//   );
+
+//   //   useEffect(() => {
+//   //     if (
+//   //       post.difficulties.length != 0 &&
+//   //       diffs.find((d) => d.id == selectedDifficulty)
+//   //     ) {
+//   //       setCanSubmit(true);
+//   //     } else {
+//   //       setCanSubmit(false);
+//   //     }
+
+//   //     console.log(canSubmit);
+//   //   }, [post]);
+
+//   //   useEffect(() => {
+//   //     post.beatmapset_id = selectedBeatmap;
+
+//   //     if (
+//   //       post.difficulties.length != 0 &&
+//   //       diffs.find((d) => d.id == selectedDifficulty)
+//   //     ) {
+//   //       setCanSubmit(true);
+//   //     } else {
+//   //       setCanSubmit(false);
+//   //     }
+//   //   }, [selectedBeatmap]);
+
+//   //   useEffect(() => {
+//   //     if (
+//   //       post.difficulties.length != 0 &&
+//   //       diffs.find((d) => d.id == selectedDifficulty)
+//   //     ) {
+//   //       setCanSubmit(true);
+//   //     } else {
+//   //       setCanSubmit(false);
+//   //     }
+//   //   }, [selectedDifficulty]);
+
+//   function closePanel() {
+//     window.history.replaceState(null, document.title, `/gd`);
+//     context.setOpen(false);
+//   }
+
+//   useEffect(() => {
+//     const beatmap_id = new URLSearchParams(location.search).get("b");
+
+//     if (!context.gd && beatmap_id && context.open) {
+//       fetch(`/api/gd/${beatmap_id}`)
+//         .then((r) => r.json())
+//         .then((d) => {
+//           if (d.status != 200)
+//             return enqueueSnackbar(d.message, {
+//               variant: "error",
+//               action,
+//             });
+
+//           context.setGD(d.data);
+//         });
+//     }
+
+//     if (context.open && context.gd) {
+//       window.history.replaceState(
+//         null,
+//         document.title,
+//         `/gd?b=${context.gd.beatmapset_id}`
+//       );
+//     }
+
+//     if (!context.open) {
+//       setSelectedDifficulty(-1);
+//     }
+//   }, [context]);
+
+//   const tabs = [
+//     <SelectDifficulty
+//       selectedDifficulty={selectedDifficulty}
+//       setSelectedDifficulty={setSelectedDifficulty}
+//       setTab={setTab}
+//     />,
+//     // <SelectBeatmap
+//     //   beatmaps={beatmaps}
+//     //   setTab={setTab}
+//     //   selected={selectedBeatmap}
+//     //   setSelected={setSelectedBeatmap}
+//     //   post={post}
+//     //   setPost={setPost}
+//     // ></SelectBeatmap>,
+//     // <PostBeatmap
+//     //   post={post}
+//     //   setPost={setPost}
+//     //   setTab={setTab}
+//     //   selectedDifficulty={selectedDifficulty}
+//     //   setSelectedDifficulty={setSelectedDifficulty}
+//     //   diffs={diffs}
+//     //   setDiffs={setDiffs}
+//     //   canSubmit={canSubmit}
+//     // />,
+//     // <CheckoutTab
+//     //   post={post}
+//     //   setPost={setPost}
+//     //   loading={loading}
+//     //   setLoading={setLoading}
+//     //   submit={submitPost}
+//     // />,
+//   ];
+
+//   //   function submitPost() {
+//   //     if (post.genres.length == 0) {
+//   //       return enqueueSnackbar("Add genres before!", {
+//   //         variant: "error",
+//   //         action,
+//   //       });
+//   //     }
+
+//   //     setLoading(true);
+
+//   //     fetch("/api/gd/new", {
+//   //       method: "POST",
+//   //       headers: {
+//   //         authorization: login.account_token,
+//   //         "content-type": "application/json",
+//   //       },
+//   //       body: JSON.stringify(post),
+//   //     })
+//   //       .then((r) => r.json())
+//   //       .then((d) => {
+//   //         enqueueSnackbar(d.message, {
+//   //           variant: d.status == 200 ? "success" : "error",
+//   //           action,
+//   //         });
+
+//   //         setLoading(false);
+//   //       });
+//   //   }
+
+//   return (
+//     <>
+//       <div className={context.open ? "gdpanel open" : "gdpanel"}>
+//         <div className={context.gd ? "container" : "container waiting"}>
+//           <div className="paneltitle">
+//             Guest Difficulty
+//             <FontAwesomeIcon
+//               icon={faTimes}
+//               color="#fff"
+//               onClick={closePanel}
+//               style={{
+//                 display: "block",
+//                 marginLeft: "auto",
+//               }}
+//             />
+//           </div>
+//           {tabs[0]}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 export default () => {
-  const [tab, setTab] = useState(0);
-  const [beatmaps, setBeatmaps] = useState([]);
-  const [offset, setOffset] = useState(0);
-  const [selectedBeatmap, setSelectedBeatmap] = useState(0);
-  const { login, setLogin } = useContext(AuthContext);
-
-  const [selectedDifficulty, setSelectedDifficulty] = useState(-1);
-  const [canSubmit, setCanSubmit] = useState(false);
-  const context = useContext(GDPanelContext);
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  const [loading, setLoading] = useState(false);
-
-  const action = (key) => (
-    <>
-      <button
-        onClick={() => {
-          closeSnackbar(key);
-        }}
-      >
-        X
-      </button>
-    </>
-  );
-
-  //   useEffect(() => {
-  //     if (
-  //       post.difficulties.length != 0 &&
-  //       diffs.find((d) => d.id == selectedDifficulty)
-  //     ) {
-  //       setCanSubmit(true);
-  //     } else {
-  //       setCanSubmit(false);
-  //     }
-
-  //     console.log(canSubmit);
-  //   }, [post]);
-
-  //   useEffect(() => {
-  //     post.beatmapset_id = selectedBeatmap;
-
-  //     if (
-  //       post.difficulties.length != 0 &&
-  //       diffs.find((d) => d.id == selectedDifficulty)
-  //     ) {
-  //       setCanSubmit(true);
-  //     } else {
-  //       setCanSubmit(false);
-  //     }
-  //   }, [selectedBeatmap]);
-
-  //   useEffect(() => {
-  //     if (
-  //       post.difficulties.length != 0 &&
-  //       diffs.find((d) => d.id == selectedDifficulty)
-  //     ) {
-  //       setCanSubmit(true);
-  //     } else {
-  //       setCanSubmit(false);
-  //     }
-  //   }, [selectedDifficulty]);
-
-  function closePanel() {
-    window.history.replaceState(null, document.title, `/gd`);
-    context.setOpen(false);
-  }
-
-  useEffect(() => {
-    const beatmap_id = new URLSearchParams(location.search).get("b");
-
-    if (!context.gd && beatmap_id && context.open) {
-      fetch(`/api/gd/${beatmap_id}`)
-        .then((r) => r.json())
-        .then((d) => {
-          if (d.status != 200)
-            return enqueueSnackbar(d.message, {
-              variant: "error",
-              action,
-            });
-
-          context.setGD(d.data);
-        });
-    }
-
-    if (context.open && context.gd) {
-      window.history.replaceState(
-        null,
-        document.title,
-        `/gd?b=${context.gd.beatmapset_id}`
-      );
-    }
-
-    if (!context.open) {
-      setSelectedDifficulty(-1);
-    }
-  }, [context]);
-
-  const tabs = [
-    <SelectDifficulty
-      selectedDifficulty={selectedDifficulty}
-      setSelectedDifficulty={setSelectedDifficulty}
-      setTab={setTab}
-    />,
-    // <SelectBeatmap
-    //   beatmaps={beatmaps}
-    //   setTab={setTab}
-    //   selected={selectedBeatmap}
-    //   setSelected={setSelectedBeatmap}
-    //   post={post}
-    //   setPost={setPost}
-    // ></SelectBeatmap>,
-    // <PostBeatmap
-    //   post={post}
-    //   setPost={setPost}
-    //   setTab={setTab}
-    //   selectedDifficulty={selectedDifficulty}
-    //   setSelectedDifficulty={setSelectedDifficulty}
-    //   diffs={diffs}
-    //   setDiffs={setDiffs}
-    //   canSubmit={canSubmit}
-    // />,
-    // <CheckoutTab
-    //   post={post}
-    //   setPost={setPost}
-    //   loading={loading}
-    //   setLoading={setLoading}
-    //   submit={submitPost}
-    // />,
-  ];
-
-  //   function submitPost() {
-  //     if (post.genres.length == 0) {
-  //       return enqueueSnackbar("Add genres before!", {
-  //         variant: "error",
-  //         action,
-  //       });
-  //     }
-
-  //     setLoading(true);
-
-  //     fetch("/api/gd/new", {
-  //       method: "POST",
-  //       headers: {
-  //         authorization: login.account_token,
-  //         "content-type": "application/json",
-  //       },
-  //       body: JSON.stringify(post),
-  //     })
-  //       .then((r) => r.json())
-  //       .then((d) => {
-  //         enqueueSnackbar(d.message, {
-  //           variant: d.status == 200 ? "success" : "error",
-  //           action,
-  //         });
-
-  //         setLoading(false);
-  //       });
-  //   }
-
-  return (
-    <>
-      <div className={context.open ? "gdpanel open" : "gdpanel"}>
-        <div className={context.gd ? "container" : "container waiting"}>
-          <div className="paneltitle">
-            Guest Difficulty
-            <FontAwesomeIcon
-              icon={faTimes}
-              color="#fff"
-              onClick={closePanel}
-              style={{
-                display: "block",
-                marginLeft: "auto",
-              }}
-            />
-          </div>
-          {tabs[0]}
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
--- src/components/gd/ManageGDPanel.tsx
+++ src/components/gd/ManageGDPanel.tsx
@@ -1,140 +1,143 @@
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useContext, useEffect, useState } from "react";
-import { GenerateComponentKey } from "../../helpers/GenerateComponentKey";
-import { AuthContext } from "../../providers/AuthContext";
-import { ManageGDPanelContext } from "../../providers/ManageGDPanelContext";
-import "./../../styles/ManageGDPanel.css";
-import GDPostSelector from "./GDPostSelector";
-import ClaimRequestsTab from "./managegdpanel/ClaimRequestsTab";
-import PostDifficultiesTab from "./managegdpanel/PostDifficultiesTab";
-import SelectPostTab from "./managegdpanel/SelectPostTab";
+// import { faTimes } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { useContext, useEffect, useState } from "react";
+// import { GenerateComponentKey } from "../../helpers/GenerateComponentKey";
+// import { AuthContext } from "../../providers/AuthContext";
+// import { ManageGDPanelContext } from "../../providers/ManageGDPanelContext";
+// import "./../../styles/ManageGDPanel.css";
+// import GDPostSelector from "./GDPostSelector";
+// import ClaimRequestsTab from "./managegdpanel/ClaimRequestsTab";
+// import PostDifficultiesTab from "./managegdpanel/PostDifficultiesTab";
+// import SelectPostTab from "./managegdpanel/SelectPostTab";
 
+// export default () => {
+//   const context = useContext(ManageGDPanelContext);
+//   const { login, setLogin } = useContext(AuthContext);
+
+//   const [tab, setTab] = useState(0);
+//   const [selectedPost, setSelectedPost] = useState("");
+//   const [selectedDifficulty, setSelectedDifficulty] = useState("");
+//   const [selectedRequest, setSelectedRequest] = useState({});
+//   const [claimRequests, setClaimRequests] = useState([]);
+//   const [selectedPostData, setSelectedPostData] = useState();
+//   const [requests, setRequests] = useState({
+//     pending: [],
+//     other: [],
+//   });
+
+//   function closePanel() {
+//     context.setOpen(false);
+//   }
+
+//   useEffect(() => {
+//     window.addEventListener("keydown", (ev) => {
+//       if (ev.key == "Escape") closePanel();
+//     });
+
+//     fetch(`/api/users/${login._id}/posts`, {
+//       headers: {
+//         authorization: login.account_token,
+//       },
+//     })
+//       .then((r) => r.json())
+//       .then((d) => {
+//         setRequests(d.data);
+//       });
+//   }, []);
+
+//   function selectPost(v: string) {
+//     setSelectedRequest(selectedRequest);
+//     setSelectedPost(v);
+//   }
+
+//   const tabs = [
+//     <SelectPostTab
+//       requests={requests}
+//       selectedPost={selectedPost}
+//       setSelectedPost={setSelectedPost}
+//       setTab={setTab}
+//       setSelectedPostData={setSelectedPostData}
+//     ></SelectPostTab>,
+//     <PostDifficultiesTab
+//       requests={requests}
+//       selectedPost={selectedPost}
+//       setSelectedPost={selectPost}
+//       selectedRequest={selectedRequest}
+//       setSelectedRequest={setSelectedRequest}
+//       setTab={setTab}
+//       claimRequests={claimRequests}
+//       setClaimRequests={setClaimRequests}
+//       setSelectedPostData={setSelectedPostData}
+//       selectedPostData={selectedPostData}
+//       setSelectedDifficulty={setSelectedDifficulty}
+//       selectedDifficulty={setSelectedDifficulty}
+//     ></PostDifficultiesTab>,
+//     <ClaimRequestsTab
+//       requests={requests}
+//       selectedPost={selectedPost}
+//       setSelectedPost={selectPost}
+//       selectedRequest={selectedRequest}
+//       setSelectedRequest={setSelectedRequest}
+//       setTab={setTab}
+//       claimRequests={claimRequests}
+//       setClaimRequests={setClaimRequests}
+//       setSelectedPostData={setSelectedPostData}
+//       selectedPostData={selectedPostData}
+//       selectedDifficulty={selectedDifficulty}
+//     ></ClaimRequestsTab>,
+//   ];
+
+//   return (
+//     <>
+//       <div className={context.open ? "managegdpanel open" : "managegdpanel"}>
+//         <div className={"container"}>
+//           <div className="paneltitle">
+//             Post Requests
+//             <FontAwesomeIcon
+//               icon={faTimes}
+//               onClick={closePanel}
+//               color="#fff"
+//               style={{
+//                 display: "block",
+//                 marginLeft: "auto",
+//               }}
+//             />
+//           </div>
+//           <div className="tab" key={GenerateComponentKey(20)}>
+//             <div
+//               className={tab == 0 ? "option selected" : "option"}
+//               onClick={() => {
+//                 setTab(0);
+//               }}
+//             >
+//               Select Post
+//             </div>
+//             <div
+//               className={tab == 1 ? "option selected" : "option"}
+//               onClick={() => {
+//                 if (!selectedPostData) return;
+//                 setTab(1);
+//               }}
+//             >
+//               Select Difficulty
+//             </div>
+//             <div
+//               className={tab == 2 ? "option selected" : "option"}
+//               onClick={() => {
+//                 if (!selectedDifficulty) return;
+
+//                 setTab(2);
+//               }}
+//             >
+//               Claim Requests
+//             </div>
+//           </div>
+//           {tabs[tab]}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 export default () => {
-  const context = useContext(ManageGDPanelContext);
-  const { login, setLogin } = useContext(AuthContext);
-
-  const [tab, setTab] = useState(0);
-  const [selectedPost, setSelectedPost] = useState("");
-  const [selectedDifficulty, setSelectedDifficulty] = useState("");
-  const [selectedRequest, setSelectedRequest] = useState({});
-  const [claimRequests, setClaimRequests] = useState([]);
-  const [selectedPostData, setSelectedPostData] = useState();
-  const [requests, setRequests] = useState({
-    pending: [],
-    other: [],
-  });
-
-  function closePanel() {
-    context.setOpen(false);
-  }
-
-  useEffect(() => {
-    window.addEventListener("keydown", (ev) => {
-      if (ev.key == "Escape") closePanel();
-    });
-
-    fetch(`/api/users/${login._id}/posts`, {
-      headers: {
-        authorization: login.account_token,
-      },
-    })
-      .then((r) => r.json())
-      .then((d) => {
-        setRequests(d.data);
-      });
-  }, []);
-
-  function selectPost(v: string) {
-    setSelectedRequest(selectedRequest);
-    setSelectedPost(v);
-  }
-
-  const tabs = [
-    <SelectPostTab
-      requests={requests}
-      selectedPost={selectedPost}
-      setSelectedPost={setSelectedPost}
-      setTab={setTab}
-      setSelectedPostData={setSelectedPostData}
-    ></SelectPostTab>,
-    <PostDifficultiesTab
-      requests={requests}
-      selectedPost={selectedPost}
-      setSelectedPost={selectPost}
-      selectedRequest={selectedRequest}
-      setSelectedRequest={setSelectedRequest}
-      setTab={setTab}
-      claimRequests={claimRequests}
-      setClaimRequests={setClaimRequests}
-      setSelectedPostData={setSelectedPostData}
-      selectedPostData={selectedPostData}
-      setSelectedDifficulty={setSelectedDifficulty}
-      selectedDifficulty={setSelectedDifficulty}
-    ></PostDifficultiesTab>,
-    <ClaimRequestsTab
-      requests={requests}
-      selectedPost={selectedPost}
-      setSelectedPost={selectPost}
-      selectedRequest={selectedRequest}
-      setSelectedRequest={setSelectedRequest}
-      setTab={setTab}
-      claimRequests={claimRequests}
-      setClaimRequests={setClaimRequests}
-      setSelectedPostData={setSelectedPostData}
-      selectedPostData={selectedPostData}
-      selectedDifficulty={selectedDifficulty}
-    ></ClaimRequestsTab>,
-  ];
-
-  return (
-    <>
-      <div className={context.open ? "managegdpanel open" : "managegdpanel"}>
-        <div className={"container"}>
-          <div className="paneltitle">
-            Post Requests
-            <FontAwesomeIcon
-              icon={faTimes}
-              onClick={closePanel}
-              color="#fff"
-              style={{
-                display: "block",
-                marginLeft: "auto",
-              }}
-            />
-          </div>
-          <div className="tab" key={GenerateComponentKey(20)}>
-            <div
-              className={tab == 0 ? "option selected" : "option"}
-              onClick={() => {
-                setTab(0);
-              }}
-            >
-              Select Post
-            </div>
-            <div
-              className={tab == 1 ? "option selected" : "option"}
-              onClick={() => {
-                if (!selectedPostData) return;
-                setTab(1);
-              }}
-            >
-              Select Difficulty
-            </div>
-            <div
-              className={tab == 2 ? "option selected" : "option"}
-              onClick={() => {
-                if (!selectedDifficulty) return;
-
-                setTab(2);
-              }}
-            >
-              Claim Requests
-            </div>
-          </div>
-          {tabs[tab]}
-        </div>
-      </div>
-    </>
-  );
+  return <></>;
 };
